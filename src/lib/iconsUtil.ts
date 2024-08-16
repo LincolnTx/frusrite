2f1da677ec658e25dfa2ff72e85350ca0b35b1af
Lincoln Teixeira <lincolnsf98@gmail.com>
2024-08-16 15:39:00 -0300

    refactor: create a coponents for all the badges Handling badges colors and incos

---
+++ src/lib/iconsUtil.ts
@@ -0,0 +1,10 @@
+export const colorHandler = {
+    indigo: 'text-indigo-500',
+    teal: 'text-teal-600'
+}
+
+interface iconStyleProps {
+    color: 'indigo' | 'teal';
+}
+
+export type { iconStyleProps as iconsColorProps };
