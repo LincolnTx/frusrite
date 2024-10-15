d5c41c1e6ec0a0dd2d93af2007c196774f2663e3
Lincoln Teixeira <lincolnsf98@gmail.com>
2024-10-15 12:29:43 -0300

    refactor: created a entry for each pedal under content/produts
    refactores index products to work with the new format

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,23 +2,22 @@ import { defineCollection, z } from "astro:content";
 
 const productCollection = defineCollection({
     type: 'data',
-    schema: z.array(
-        z.object({
+    schema:z.object({
+        id: z.number(),
+        title: z.string(),
+        image: z.string(),
+        summary: z.string(),
+        'main-image': z.string(),
+        gallery: z.array(z.string()),
+        reviews: z.array(z.string()),
+        extra: z.object({
             title: z.string(),
-            image: z.string(),
-            summary: z.string(),
-            'main-image': z.string(),
-            gallery: z.array(z.string()),
-            reviews: z.array(z.string()),
-            extra: z.object({
-                title: z.string(),
-                content: z.string()
-            }),
-            // TODO: verificar a possibilide de transformar o em array
-            // cada item seria um paragrafo
-            description: z.string()
-        })
-    ),
+            content: z.string()
+        }),
+        // TODO: verificar a possibilide de transformar o em array
+        // cada item seria um paragrafo
+        description: z.string()
+    }),
 });
 
 export const collections  = {
